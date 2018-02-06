f9b80c111c4bc8408bd14b43125a53395a0e2109
t953019 <michael.eggers@syngenta.com>
2018-02-06 13:23:13 -0500

    listed data types for whole numbers

--- ByteShortInt/src/com/eggscodes/Main.java
+++ ByteShortInt/src/com/eggscodes/Main.java
@@ -3,6 +3,19 @@ package com.eggscodes;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // this considered a literal
+        // int has width of 32
+        int myMinValue = -2_147_483_648;
+        int myMaxValue = 2_147_483_647;
+
+        // useful for lots of small calculations
+        // byte hsa width of 8
+        byte myByteValue = -128;
+
+        // short has width of 16
+        short myShortValue = -32768;
+
+        // long has width of 64
+        long myLongValue = 100L;
     }
 }
